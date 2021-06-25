bff79bd6f9301c9c948233c3af33ce69c2aba297
elliotmcs <elliot.mcsmythurs@plurilock.com>
2021-06-24 22:24:05 -0700

    Adds skeleton code for foreign exchange table

---
+++ src/main/webapp/app/entities/foreign-exchange/currency.reducer.tsx
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import { ICrudGetAllAction } from 'react-jhipster';
+import { REQUEST, SUCCESS, FAILURE } from 'app/shared/reducers/action-type.util';
+import { ICurrency, defaultValue } from 'app/shared/model/currency.model';
+
+export const ACTION_TYPES = {
+  FETCH_CURRENCIES_LIST: 'currency/FETCH_CURRENCIES_LIST',
+  FETCH_CURRENCIES: 'currency/FETCH_CURRENCIES'
+};
+
+const initialState = {
+  loading: false,
+  errorMessage: null,
+  entities: [] as ReadonlyArray<ICurrency>,
+  entity: defaultValue,
+  totalItems: 0
+};
+
+export type CurrenciesState = Readonly<typeof initialState>;
+
+// Reducer
+
+export default (state: CurrenciesState = initialState, action): CurrenciesState => {
+  switch (action.type) {
+    case REQUEST(ACTION_TYPES.FETCH_CURRENCIES_LIST):
+    case REQUEST(ACTION_TYPES.FETCH_CURRENCIES):
+      return {
+        ...state,
+        errorMessage: null,
+        loading: true
+      };
+    case FAILURE(ACTION_TYPES.FETCH_CURRENCIES_LIST):
+    case FAILURE(ACTION_TYPES.FETCH_CURRENCIES):
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.payload
+      };
+    case SUCCESS(ACTION_TYPES.FETCH_CURRENCIES_LIST):
+      return {
+        ...state,
+        loading: false,
+        entities: action.payload.data,
+        totalItems: parseInt(action.payload.headers['x-total-count'], 10)
+      };
+    case SUCCESS(ACTION_TYPES.FETCH_CURRENCIES):
+      return {
+        ...state,
+        loading: false,
+        entity: action.payload.data
+      };
+    default:
+      return state;
+  }
+};
+
+// Actions
+
+export const getEntities: ICrudGetAllAction<ICurrency> = (page, size, sort) => ({
+  type: ACTION_TYPES.FETCH_CURRENCIES_LIST,
+  payload: axios.get('api/transactions').then(res => {
+    const c1: ICurrency = { currencyName: 'USD', exchangeRate: 0.81 };
+    const c2: ICurrency = { currencyName: 'CNY', exchangeRate: 5.27 };
+    const c3: ICurrency = { currencyName: 'JPY', exchangeRate: 90.23 };
+    const c4: ICurrency = { currencyName: 'EUR', exchangeRate: 0.68 };
+    const c5: ICurrency = { currencyName: 'INR', exchangeRate: 60.29 };
+    const c6: ICurrency = { currencyName: 'GBP', exchangeRate: 0.58 };
+    res.data = [c1, c2, c3, c4, c5, c6];
+    return res;
+  })
+});
